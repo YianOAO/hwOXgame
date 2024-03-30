7be22c2698e21c2d49dcf6032484089e0fd7e2a6
YianOAO <wjh0908178837@gmail.com>
2024-03-30 23:04:40 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace hwOXgame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            label1 = new Label();
+            button10 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonFace;
+            button1.Location = new Point(131, 69);
+            button1.Name = "button1";
+            button1.Size = new Size(90, 90);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Control;
+            button2.Location = new Point(241, 69);
+            button2.Name = "button2";
+            button2.Size = new Size(90, 90);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.Control;
+            button3.Location = new Point(351, 69);
+            button3.Name = "button3";
+            button3.Size = new Size(90, 90);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.Control;
+            button4.Location = new Point(131, 179);
+            button4.Name = "button4";
+            button4.Size = new Size(90, 90);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.Control;
+            button5.Location = new Point(241, 179);
+            button5.Name = "button5";
+            button5.Size = new Size(90, 90);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.Control;
+            button6.Location = new Point(351, 179);
+            button6.Name = "button6";
+            button6.Size = new Size(90, 90);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.Control;
+            button7.Location = new Point(351, 294);
+            button7.Name = "button7";
+            button7.Size = new Size(90, 90);
+            button7.TabIndex = 8;
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.Control;
+            button8.Location = new Point(241, 294);
+            button8.Name = "button8";
+            button8.Size = new Size(90, 90);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.Control;
+            button9.Location = new Point(131, 294);
+            button9.Name = "button9";
+            button9.Size = new Size(90, 90);
+            button9.TabIndex = 6;
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(590, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 25);
+            label1.TabIndex = 9;
+            label1.Text = "Player";
+            label1.Click += label1_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Microsoft JhengHei UI", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            button10.Location = new Point(565, 69);
+            button10.Name = "button10";
+            button10.Size = new Size(126, 125);
+            button10.TabIndex = 10;
+            button10.Text = "X";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button10);
+            Controls.Add(label1);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label label1;
+        private Button button10;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Windows.Forms;
+namespace hwOXgame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
